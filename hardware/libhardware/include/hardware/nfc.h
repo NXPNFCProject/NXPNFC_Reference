e0e1f16af73a0863c7ae65f402b61c45fdca9b65
nxpandroid <srikrishna.pramoda@nxp.com>
2016-08-01 19:43:28 +0530

    NFC_NCIHALx_ARC0.4.7.0_M_OpnSrc

--- hardware/libhardware/include/hardware/nfc.h
+++ hardware/libhardware/include/hardware/nfc.h
@@ -13,25 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/******************************************************************************
- *
- *  The original Work has been changed by NXP Semiconductors.
- *
- *  Copyright (C) 2015 NXP Semiconductors
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- ******************************************************************************/
 
 #ifndef ANDROID_NFC_HAL_INTERFACE_H
 #define ANDROID_NFC_HAL_INTERFACE_H
@@ -140,7 +121,8 @@ enum {
     HAL_NFC_IOCTL_DISABLE_HAL_LOG,
     HAL_NFC_IOCTL_NCI_TRANSCEIVE,
     HAL_NFC_IOCTL_P61_GET_ACCESS,
-    HAL_NFC_IOCTL_P61_REL_ACCESS
+    HAL_NFC_IOCTL_P61_REL_ACCESS,
+    HAL_NFC_IOCTL_P73_ISO_RST
 };
 
 /*
