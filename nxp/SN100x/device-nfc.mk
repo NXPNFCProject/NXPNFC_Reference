54f184dcbacd2a9c0874607ac7383d9bf3cf8c7a
nxf24591 <nanjesh.s_1@nxp.com>
2018-08-23 13:43:12 +0530

    NFC_NCIHALx_AR2000.09.00.0D_OpnSrc

--- nxp/SN100x/device-nfc.mk
+++ nxp/SN100x/device-nfc.mk
@@ -12,7 +12,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    vendor/$(NXP_VENDOR_DIR)/opensource/frameworks/nxp-nfc-gsma/com.gsma.services.nfc.xml:system/etc/permissions/com.gsma.services.nfc.xml
 
 # NFC config files
 PRODUCT_COPY_FILES += \
