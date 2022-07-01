7ea59131c890a6dab35b80c2384affb3461c293a
nxf24591 <nanjesh.s_1@nxp.com>
2022-07-01 21:29:03 +0530

    Updated corresponding to - NFC_AR_00_18C0_13.01.00_OpnSrc

--- nxp/pn8xt/device-nfc.mk
+++ nxp/pn8xt/device-nfc.mk
@@ -16,31 +16,35 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:system/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.sofware.nfc.beam.xml:system/etc/permissions/android.sofware.nfc.beam.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml 
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:system/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/com.android.se.xml:system/etc/permissions/com.android.se.xml \
+
 
 # SE config files
 PRODUCT_COPY_FILES += \
-    hardware/nxp/secure_element/libese-spi/p73/libese-nxp-P73.conf:vendor/etc/libese-nxp.conf
+    hardware/nxp/secure_element/pn8x/libese-spi/p73/libese-nxp-P73.conf:vendor/etc/libese-nxp.conf
 
 
 ifeq ($(NXP_NFC_HW),pn81T)
 PRODUCT_COPY_FILES += \
-    hardware/nxp/nfc/halimpl/libnfc-nci_NCI2_0.conf:system/product/etc/libnfc-nci.conf \
-    hardware/nxp/nfc/halimpl/libnfc-nxp-PN81T_example.conf:vendor/etc/libnfc-nxp.conf
+    hardware/nxp/nfc/pn8x/halimpl/libnfc-nci_NCI2_0.conf:system/etc/libnfc-nci.conf \
+    hardware/nxp/nfc/pn8x/halimpl/libnfc-nxp-PN81T_example.conf:vendor/etc/libnfc-nxp.conf \
+    vendor/nxp/pn8xt/hw/hal_uuid_map_config.xml:vendor/etc/hal_uuid_map_config.xml
 
     ifeq ($(NXP_RF_REQ),true)
     PRODUCT_COPY_FILES += \
-        hardware/nxp/nfc/halimpl/libnfc-nxp_RF-PN81T_example.conf:vendor/libnfc-nxp_RF.conf
+        hardware/nxp/nfc/pn8x/halimpl/libnfc-nxp_RF-PN81T_example.conf:vendor/libnfc-nxp_RF.conf
     endif
 
 else
 PRODUCT_COPY_FILES += \
-    hardware/nxp/nfc/halimpl/libnfc-nci.conf:system/product/etc/libnfc-nci.conf
-    hardware/nxp/nfc/halimpl/libnfc-nxp-PN80T_example.conf:vendor/etc/libnfc-nxp.conf \
+    hardware/nxp/nfc/pn8x/halimpl/libnfc-nci.conf:system/etc/libnfc-nci.conf
+    hardware/nxp/nfc/pn8x/halimpl/libnfc-nxp-PN80T_example.conf:vendor/etc/libnfc-nxp.conf \
 
     ifeq ($(NXP_RF_REQ),true)
     PRODUCT_COPY_FILES += \
-        hardware/nxp/nfc/halimpl/libnfc-nxp_RF-PN80T_example.conf:vendor/libnfc-nxp_RF.conf
+        hardware/nxp/nfc/pn8x/halimpl/libnfc-nxp_RF-PN80T_example.conf:vendor/libnfc-nxp_RF.conf
     endif
 
 endif
@@ -49,7 +53,6 @@ endif
 PRODUCT_COPY_FILES += \
      vendor/$(NXP_VENDOR_DIR)/pn8xt/hw/init.$(NXP_NFC_PLATFORM).nfc.rc:vendor/etc/init/init.$(NXP_NFC_HOST).nfc.rc \
      vendor/$(NXP_VENDOR_DIR)/pn8xt/hw/init.$(NXP_NFC_PLATFORM).se.rc:vendor/etc/init/init.$(NXP_NFC_HOST).se.rc \
-
 # NFC packages
 PRODUCT_PACKAGES += \
     libnfc-nci \
@@ -77,4 +80,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 BOARD_SEPOLICY_DIRS += vendor/$(NXP_VENDOR_DIR)/pn8xt/sepolicy \
                        vendor/$(NXP_VENDOR_DIR)/pn8xt/sepolicy/nfc \
                        vendor/$(NXP_VENDOR_DIR)/pn8xt/sepolicy/se \
-                       vendor/$(NXP_VENDOR_DIR)/pn8xt/sepolicy/wiredse \
+		       vendor/$(NXP_VENDOR_DIR)/pn8xt/sepolicy/wiredse \
