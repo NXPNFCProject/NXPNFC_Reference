f9d3c75eb6e07730a29395b2685293752ba3519e
nxf24591 <nanjesh.s_1@nxp.com>
2025-02-14 19:08:21 +0530

    Uploaded corresponding to NFC_AR_00_7E800_16.03.00_OpnSrc

--- nxp/SNxxx/config.fs
+++ nxp/SNxxx/config.fs
@@ -7,7 +7,16 @@ value:2902
 [AID_VENDOR_NXP_AUTHSECRET]
 value:2903
 
+[AID_VENDOR_NXP_SE_UPDATE_AGENT]
+value:2904
+
 [vendor/bin/hw/android.hardware.security.keymint3-service.strongbox.nxp]
+mode: 0755
+user: AID_VENDOR_NXP_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE WAKE_ALARM
+
+[vendor/bin/hw/android.hardware.security.keymint4-service.strongbox.nxp]
 mode: 0755
 user: AID_VENDOR_NXP_STRONGBOX
 group: AID_SYSTEM
@@ -25,3 +34,8 @@ user: AID_VENDOR_NXP_AUTHSECRET
 group: AID_SYSTEM
 caps: SYS_ADMIN SYS_NICE WAKE_ALARM
 
+[vendor/bin/hw/se_update_agent.nxp]
+mode: 0755
+user: AID_VENDOR_NXP_SE_UPDATE_AGENT
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE WAKE_ALARM
--- nxp/SNxxx/device-nfc.mk
+++ nxp/SNxxx/device-nfc.mk
@@ -10,6 +10,7 @@ NXP_NFC_HW := $(TARGET_NXP_NFC_HW)
 endif
 NXP_NFC_PLATFORM := SNxxx
 NXP_VENDOR_DIR := nxp
+KM_VER_DEFAULT ?= KM300
 
 # Variable to enable/disable power tracker feature.
 # By default this is disabled. This can be enabled by either by changing below value to true
@@ -20,14 +21,18 @@ POWER_TRACKER_FEATURE ?= false
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.ese.xml:system/etc/permissions/android.hardware.nfc.ese.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:system/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:system/etc/permissions/android.hardware.se.omapi.ese.xml \
-    frameworks/native/data/etc/com.android.se.xml:system/etc/permissions/com.android.se.xml \
     frameworks/native/data/etc/android.software.secure_lock_screen.xml:system/etc/permissions/android.software.secure_lock_screen.xml \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:system/etc/permissions/android.hardware.device_unique_attestation.xml \
+
+ifneq ($(NXP_NFC_HW),PN557)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:system/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:system/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/com.android.se.xml:system/etc/permissions/com.android.se.xml
+endif
 
 # NFC config files
 ifeq ($(NXP_NFC_HW),SN1xx)
@@ -62,12 +67,15 @@ endif
 
 # NFC Init Files
 PRODUCT_COPY_FILES += \
-     vendor/$(NXP_VENDOR_DIR)/SNxxx/hw/init.$(NXP_NFC_PLATFORM).nfc.rc:vendor/etc/init/init.$(NXP_NFC_HOST).nfc.rc \
-     vendor/$(NXP_VENDOR_DIR)/SNxxx/hw/init.$(NXP_NFC_PLATFORM).se.rc:vendor/etc/init/init.$(NXP_NFC_HOST).se.rc \
+     vendor/$(NXP_VENDOR_DIR)/SNxxx/hw/init.$(NXP_NFC_PLATFORM).nfc.rc:vendor/etc/init/init.$(NXP_NFC_HOST).nfc.rc
+
+ifneq ($(NXP_NFC_HW),PN557)
+PRODUCT_COPY_FILES += \
+     vendor/$(NXP_VENDOR_DIR)/SNxxx/hw/init.$(NXP_NFC_PLATFORM).se.rc:vendor/etc/init/init.$(NXP_NFC_HOST).se.rc
+endif
 
 # Service packages
 PRODUCT_PACKAGES += \
-    NfcNci \
     SecureElement \
     Tag \
     rkpdapp \
@@ -84,6 +92,7 @@ PRODUCT_PACKAGES += \
     libnfc_wlc_jni \
     WlcServiceDefaults \
     NxpDTA \
+    TR13.2_NXPDTA \
 
 #Test Apps
 PRODUCT_PACKAGES += \
@@ -91,11 +100,16 @@ PRODUCT_PACKAGES += \
     OSUTestAPP \
     SBupdateApp \
     JrcpOmapiSmb \
+    JrcpOmapiSmb_Mainline \
     JrcpOmapiSpi \
+    JrcpOmapiSpi_AOSP \
     JrcpProxy \
+    jrcpProxyAosp \
     JrcpOmapiSemdemon \
     JrcpOmapiVISO \
-    DynamicAidNonPaymentOffHostTest \
+    PaymentApp_JRCP_dynamicOM \
+    PaymentApp_defaultOM \
+    PaymentApp_DynamicAidNonPaymentOffHostTest \
     PaymentApp_eSE \
     PaymentApp_eSE_ConflictAID \
     PaymentApp_eSE_NoConflictAID \
@@ -115,29 +129,29 @@ PRODUCT_PACKAGES += \
     PaymentApp_uicc_NoConflictAID \
     PaymentApp_uicc_Overflow \
     PaymentApp_uicc_overflow_nonpayment_aids \
-    PaymentAppUicc2 \
-    PaymentAppSE2_eSE \
-    PaymentFelicaHostApp \
+    PaymentApp_Uicc2 \
+    PaymentApp_SE2_eSE \
+    PaymentApp_FelicaHostApp \
     PaymentApp_eSE_AIDCountTest \
-    StaticDualUiccSwitch \
+    dualUiccSwitch \
     JrcpOmapi_Tee \
     JrcpOmapi_Ree \
     RssiApp \
-    PaymentLoopbackApp_uicc \
-    PaymentLoopbackApp_eSE \
+    PaymentApp_LoopbackApp_uicc \
+    PaymentApp_LoopbackApp_eSE \
     PaymentApp_host_Nunit \
     NxpTransitWallet \
-    paymentapp_ese_49AIDs \
-    paymentapp_ese_50AIDs \
-    paymentapp_uicc_49AIDs \
-    paymentapp_uicc_50AIDs \
-    PaymentFelicaHostApp_4203 \
-    FelicaHostApp_EmptyData \
-    PaymentAppSE2_eSE_AID_Filtering \
+    PaymentApp_ese_49AIDs \
+    PaymentApp_ese_50AIDs \
+    PaymentApp_uicc_49AIDs \
+    PaymentApp_uicc_50AIDs \
+    PaymentApp_FelicaHostApp_4203 \
+    PaymentApp_FelicaHostApp_EmptyData \
+    PaymentApp_eSE_AID_Filtering \
     PaymentApp_euicc \
     PaymentApp_euicc_Overflow \
     PaymentApp_Preferred \
-    Paymentapp_prefix_sufix \
+    PaymentApp_prefix_sufix \
     PaymentApp_uicc2 \
     PaymentApp_host_UL_F_SO_F \
     PaymentApp_host_UL_F_SO_T \
@@ -151,28 +165,53 @@ PRODUCT_PACKAGES += \
 
 #ADD ALL HAL Services
 PRODUCT_PACKAGES += \
+    android.hardware.nfc2-service.nxp \
+
+ifneq ($(NXP_NFC_HW),PN557)
+PRODUCT_PACKAGES += \
     android.hardware.secure_element-service.nxp \
-    android.hardware.wired_se@1.0-service \
     android.hardware.trusted_se@1.2-service \
-    android.hardware.nfc-service.nxp \
     android.hardware.authsecret-service.nxp \
     android.hardware.weaver-service.nxp
+endif
+
+#ADD ALL HAL Ext libraries
+PRODUCT_PACKAGES += \
+    libnxp_nfc_gen_ext_sys \
+    libnxp_nfc_prop_ext_sys \
+    libnxp_nfc_gen_ext_vnd \
+    libnxp_nfc_prop_ext_vnd
 
 ifeq ($(NXP_NFC_HW),SN1xx)
 # ADD keymaster HAL for SN1xx
 PRODUCT_PACKAGES += \
 android.hardware.keymaster@4.1-javacard.service \
 
-else
+else ifneq ($(NXP_NFC_HW),PN557)
 
-# ADD keymint hal for SN220
+# ADD keymint hal for SN220, SN300
 PRODUCT_PACKAGES += \
 android.hardware.security.keymint3-service.strongbox.nxp \
+android.hardware.security.keymint4-service.strongbox.nxp \
+se_update_agent.nxp
+
+
+ifeq ($(KM_VER_DEFAULT),KM300)
+PRODUCT_PACKAGES += \
+android.hardware.security.keymint3-service.strongbox.nxp.xml \
+android.hardware.security.keymint3-service.strongbox.nxp.rc \
+android.hardware.security.sharedsecret-service.strongbox3.nxp.xml
+else
+PRODUCT_PACKAGES += \
+android.hardware.security.keymint4-service.strongbox.nxp.xml \
+android.hardware.security.keymint4-service.strongbox.nxp.rc \
+android.hardware.security.sharedsecret-service.strongbox4.nxp.xml
+endif #KM_VER_DEFAULT
 
 endif
 
 
-#VTS/gtest native test modules
+#VTS/gtest/atest native test modules
 PRODUCT_PACKAGES += \
     VtsAidlHalNfcTargetTest \
     VtsHalSecureElementTargetTest \
@@ -186,7 +225,13 @@ PRODUCT_PACKAGES += \
     SelfTestHalAidlNfc \
     VtsHalOmapiSeAccessControlTestCases \
     VtsAidlKeyMintBenchmarkTest \
-    
+    nxp_nfc_hal_gtest \
+    libnfc-nci-tests \
+    NfcNciInstrumentationTests \
+    NfcNciUnitTests \
+    NfcManagerTests \
+    NfcTestCases \
+
 ifeq ($(ENABLE_TREBLE), true)
 PRODUCT_PACKAGES += \
 	vendor.nxp.nxpnfc@1.0-impl \
@@ -208,8 +253,12 @@ PRODUCT_PRODUCT_PROPERTIES += ro.product.name=unknown
 PRODUCT_MODEL_FOR_ATTESTATION := $(TARGET_PRODUCT)
 PRODUCT_BRAND_FOR_ATTESTATION := Android
 
+ifneq ($(NXP_NFC_HW),PN557)
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+endif
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 BOARD_SEPOLICY_DIRS += vendor/$(NXP_VENDOR_DIR)/SNxxx/sepolicy \
@@ -228,7 +277,7 @@ BOARD_SEPOLICY_DIRS += vendor/$(NXP_VENDOR_DIR)/SNxxx/sepolicy/internal/trusteds
 
 ifeq ($(POWER_TRACKER_FEATURE), true)
 PRODUCT_PACKAGES += android.hardware.power.stats-service.pixel \
-                    power_tracker \
+                    power_tracker_v2 \
                     VtsHalPowerStatsTargetNfcTest
 
 BOARD_SEPOLICY_DIRS += vendor/$(NXP_VENDOR_DIR)/SNxxx/sepolicy/nfc/power-tracker
@@ -239,6 +288,11 @@ PRODUCT_SOONG_NAMESPACES += hardware/google/pixel \
                             device/google/gs-common/powerstats \
                             device/google/gs101/powerstats \
                             hardware/nxp/tests/powerstats
+endif
+
+ifneq ($(NXP_NFC_HW),PN557)
+PRODUCT_PACKAGES += WiredSe
+BOARD_SEPOLICY_DIRS += vendor/$(NXP_VENDOR_DIR)/SNxxx/sepolicy/nfc/wired-se
 endif
 
 PRODUCT_SYSTEM_PROPERTIES += ro.boot.hardware.sku=NXP-NFC
